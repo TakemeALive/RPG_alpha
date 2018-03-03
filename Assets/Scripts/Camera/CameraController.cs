ac0f74452fed1a9e4385ba9b0ccd98b4daa04f4f
Hugo <>
2018-03-03 19:26:48 +0900

    Initialized Game based on Official Tutorial "Roll a ball" : 1.Generates pickup cubes by a ItemManager with random positions. // TODO: cubes overlap with walls(ItemManager.cs) 2.DataManager manages static Datas(paths of files, amount of cubes). 3.GameManager manages comprehensive game play.

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject _player;
+
+	private Vector3 _offset;
+
+	// Use this for initialization
+	void Start () {
+		_offset = transform.position - _player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = _player.transform.position + _offset;
+	}
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public Rigidbody _rigidbody;
+	public float _speed = 10.0f;
+
+	void Start()
+	{
+		_rigidbody = GetComponent<Rigidbody>();		
+	}
+
+	void FixedUpdate()
+	{
+		if(_rigidbody.velocity.y <= 1.0f)
+		{
+			float moveHorizontal = Input.GetAxis("Horizontal");
+			float moveVertical = Input.GetAxis("Vertical");
+
+			Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+			_rigidbody.AddForce(movement * _speed);
+		}
+	}
+	private void OnTriggerEnter(Collider other) {
+		if(other.gameObject.CompareTag("PickupCube"))
+		{
+			other.gameObject.SetActive(false);
+		}
+	}
+}
