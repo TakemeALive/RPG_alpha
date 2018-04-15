e36814640284a4a01dedfec313ca409513c691e5
Hugo <>
2018-04-15 23:54:20 +0900

    3 types of enemies are now spawning in 3 different spawning points.

---
+++ Assets/Scripts/Managers/EnemyManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour {
+
+	public PlayerHealth playerHealth;
+	public float spawnTime = 3f;
+	public GameObject[] enemyArray;
+	public Transform[] spawnPoints;
+	
+	void Start()
+	{
+		InvokeRepeating("Spawn", spawnTime, spawnTime);
+	}
+
+	void Spawn()
+	{
+		if(playerHealth.currentHealth <= 0f)
+		{
+			return;
+		}
+
+		int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+		int enemyIndex = Random.Range(0, enemyArray.Length);
+		Instantiate(enemyArray[enemyIndex], spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+	}
+}
